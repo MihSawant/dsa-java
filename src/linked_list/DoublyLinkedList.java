347ae37a9bf033369d8c831abadc7f779f79c72a
Mihir Sawant <mihirssawant06@gmail.com>
2022-12-04 17:23:33 +0530

    Added code for doubly linked list insertion operation

---
+++ src/linked_list/DoublyLinkedList.java
@@ -0,0 +1,75 @@
+package linked_list;
+
+public class DoublyLinkedList {
+    static class Node{
+        int data;
+        Node prv;
+        Node next;
+
+        public String toString(){
+            return ""+data;
+        }
+    }
+
+    Node head;
+    Node tail;
+    int size;
+
+    public void insertFirst(int data){
+        Node newNode = new Node();
+        newNode.data = data;
+
+        newNode.next = head;
+        head = newNode;
+
+        if(tail == null){
+            tail = head;
+        }
+        size++;
+    }
+
+    public void insertLast(int data){
+        if(tail == null){
+            insertFirst(data);
+        } else{
+            Node newNode = new Node();
+            newNode.data = data;
+
+            newNode.prv = tail;
+            tail.next = newNode;
+
+            tail = newNode;
+            size++;
+        }
+    }
+
+    public void insertAfter(int n, int value){
+       Node node = head;
+       while(node != null){
+           if(node.data == n){
+               Node newNode = new Node();
+               newNode.data = value;
+
+              Node next = node.next;
+               newNode.prv = node;
+               newNode.next = next;
+               next.prv = newNode;
+               node.next = newNode;
+               size++;
+           }
+           node = node.next;
+       }
+    }
+
+    public void display(){
+        Node node = head;
+        while(node != null){
+            System.out.println(node);
+            node = node.next;
+        }
+    }
+
+    public int getSize(){
+        return size;
+    }
+}
---
+++ src/linked_list/DoublyLinkedListMain.java
@@ -0,0 +1,19 @@
+package linked_list;
+
+public class DoublyLinkedListMain {
+    public static void main(String[] args) {
+        DoublyLinkedList dll = new DoublyLinkedList();
+        dll.insertFirst(10);
+        dll.insertLast(20);
+        dll.insertLast(30);
+        dll.insertLast(40);
+        dll.insertLast(50);
+
+        dll.display();
+        System.out.println("Insert after node 40 45");
+        dll.insertAfter(40, 45);
+        dll.display();
+
+        if(dll.getSize() != 6) throw new AssertionError("Size must be 6 as new Node is added in between");
+    }
+}
