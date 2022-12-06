249d7322b0b29f5011fffcfbd6646c14dce83494
Mihir Sawant <mihirssawant06@gmail.com>
2022-12-06 09:43:36 +0530

    trying another example in bin tree

--- src/tree/bin_tree/BinaryTreeMain.java
+++ src/tree/bin_tree/BinaryTreeMain.java
@@ -2,15 +2,29 @@ package tree.bin_tree;
 
 public class BinaryTreeMain {
     public static void main(String[] args) {
-        BinaryTree<Integer> tree = new BinaryTree<>();
+//        BinaryTree<Integer> tree = new BinaryTree<>();
+          BinaryTree<Character> btree = new BinaryTree<>();
+
+//       tree.insert(10);
+//       tree.insert(20);
+//       tree.insert(30);
+//       tree.insert(40);
+//
+//       tree.inOrderTraversal();
+//       tree.getTotalNodes();
+       // trying another example
+        btree.insert('A');
+        btree.insert('B');
+        btree.insert('C');
+        btree.insert('D');
+        btree.insert('E');
+        btree.insert('F');
+        btree.insert('G');
+        btree.insert('H');
+        btree.insert('I');
 
 
-       tree.insert(10);
-       tree.insert(20);
-       tree.insert(30);
-       tree.insert(40);
-
-       tree.inOrderTraversal();
-       tree.getTotalNodes();
+        btree.getTotalNodes();
+        btree.preOrderTraversal();
     }
 }
