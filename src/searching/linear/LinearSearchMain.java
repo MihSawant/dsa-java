7a021c878fea0d9730da56a8cd62059ab2d89f3e
Mihir Sawant <mihirssawant06@gmail.com>
2022-12-06 13:55:27 +0530

    testing with different data arrays

--- src/searching/linear/LinearSearchMain.java
+++ src/searching/linear/LinearSearchMain.java
@@ -4,7 +4,10 @@ public class LinearSearchMain {
     public static void main(String[] args) {
         LinearSearch ls = new LinearSearch();
         Integer[] numbers = {3, 5, 10, 45, 56, 21};
-        int res = ls.search(numbers, 56);
+        String[] data = {"Mihir", "Yash", "Hari", "Shayan"};
+//        int res = ls.search(numbers, 56);
+         int res = ls.search(data, "Mihir");
+         res = ls.search(data, "Aayan");
         if(res != -1){
             System.out.println("Found at pos: "+res);
         } else{
